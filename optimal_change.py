3b7daba87fe390e694730284addd805bd033828d
Miah Clay <miahb88@gmail.com>
2022-06-03 17:34:00 -0400

    solved but trying to figure out how to test with spec file

--- optimal_change.py
+++ optimal_change.py
@@ -16,25 +16,42 @@ def optimal_change(item_cost, amount_paid):
         'nickel': .05,
         'penny': .01
     }
+    # print(str(item_cost))
+    # print(amount_paid)
+    # print(str(amount_paid))
     change_arr = []
     answer = ""
-    change_due = amount_paid % item_cost
+    change_due = amount_paid - item_cost
     cost = 0 + item_cost
     # print(cost)
     if item_cost == 0:
-        return (f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is ${amount_paid}")
+        if amount_paid == 0:    
+            item_cost = format(item_cost,".2f")
+            amount_paid = format(amount_paid,".2f")
+            return (f"This item is on the house!")
+        if amount_paid > 0:    
+            item_cost = format(item_cost,".2f")
+            amount_paid = format(amount_paid,".2f")
+            return (f"This item is on the house, keep your ${amount_paid}.")
     
+    elif item_cost == amount_paid:
+        return "Thanks for using exact change.  Have a great day!"
+
     elif item_cost > amount_paid:
-        return (f"Sorry, you don't have enough money.  This item costs ${item_cost} and you only gave ${amount_paid}")
+        item_cost = format(item_cost,".2f")
+        amount_paid = format(amount_paid,".2f")
+        return (f"Sorry, you don't have enough money.  This item costs ${item_cost} and you only gave ${amount_paid}.")
     
     else:
         for x in coins:
             coin_value = coins[x]
             coin_name = x
         # print(coin_name, coin_value) # to verify dictionary order is correct
+            change_due = round(change_due,2)
+            # return type(change_due)
             even_div = change_due / coin_value
             # print(f"even_div {even_div} = change due {change_due} / bill {coin_value} ")
-            # print(even_div)
+            # print(x)
             if even_div >= 1:
                 ind_arr = []
                 ind_arr.append(math.floor(even_div))
@@ -43,6 +60,7 @@ def optimal_change(item_cost, amount_paid):
                 change_due -= (math.floor(even_div) * coin_value)
             else:
                 continue
+        # print(change_arr)
         # identify need for plural vs singular    
         for y in change_arr:
             # print(y)
@@ -55,34 +73,52 @@ def optimal_change(item_cost, amount_paid):
                     # print(y)
             else:
                 continue
+        # print(change_arr)
         for x in change_arr:
             x[0] = str(x[0])
             # print(x[0])
             new = " ".join(x)
-            if x != change_arr[(len(change_arr))-1]:
-                answer += new
-                answer += ", "
+            if len(change_arr) > 1:
+                if x != change_arr[(len(change_arr))-1]:
+                    answer += new
+                    answer += ", "
+                else:
+                    answer += ("and " + new + ".")
             else:
-                answer += ("and " + new + ".")
+                answer += (new + ".")
                 # answer += "."
             # print(answer)
         # answer = " ".join(change_arr)
         # print(answer)
     # return change_arr
+        item_cost = format(item_cost,".2f")
+        amount_paid = format(amount_paid,".2f")
         return(f"The optimal change for an item that costs ${item_cost} with an amount paid of ${amount_paid} is {answer}")
 
 
 
 
-
+# Need to:
+#     * fix zeroes in numbers (DONE)
+#     * fix penny issue (DONE)
 
 # TESTS 
 print(optimal_change(62.13, 100)) #37.87
-# print(optimal_change(31.51, 50)) 
-# print(optimal_change(1,5)) 
-# print(optimal_change(4,5)) 
+print(optimal_change(31.51, 50)) 
+print(optimal_change(1,5)) 
+print(optimal_change(4,5)) 
+print(optimal_change(100.00, 500.00))
+print(optimal_change(20,120))
+
+print(optimal_change(5,5))
+print(optimal_change(0.00,5.00)) 
+print(optimal_change(5.00,4.00))
+print(optimal_change(0.00,0.00)) 
+print(optimal_change(.01,100.00))
 
 
 # print("1:", optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
 
 # print("2:", optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
+
+# export function optimal_change()
--- optimal_change_spec.py
+++ optimal_change_spec.py
@@ -1,17 +1,28 @@
+import optimal_change() from optimal_change
+
+
+
 # Write your tests here!
-print(optimal_change(62.13, 100) == )
-print(optimal_change(31.51, 50) == )
-print(optimal_change(1,5) == )
-print(optimal_change(4,5) == )
+print(optimal_change(62.13, 100) == "The optimal change for an item that costs $62.13 with an amount paid of $100.00 is 1 $20 bill, 1 $10 bill, 1 $5 bill, 2 $1 bills, 3 quarters, 1 dime, and 2 pennies.")
 
-print(optimal_change(1,5) == )
-print(optimal_change(1,5) == )
-print(optimal_change(1,5) == )
-print(optimal_change(1,5) == )
-print(optimal_change(1,5) == )
+print(optimal_change(31.51, 50) == "The optimal change for an item that costs $31.51 with an amount paid of $50.00 is 1 $10 bill, 1 $5 bill, 3 $1 bills, 1 quarter, 2 dimes, and 4 pennies.")
 
-# Edge cases:
-print(optimal_change(5,4.99) == ) # not enough money, you're broke!'
-print(optimal_change(.01,100) == ) # lots of change
-print(optimal_change(0,5) == ) # no cash given
-print(optimal_change(0,0) == ) # it's free, doesn't cost anything
+print(optimal_change(1,5) == "The optimal change for an item that costs $1.00 with an amount paid of $5.00 is 4 $1 bills.")
+
+print(optimal_change(4,5) == "The optimal change for an item that costs $4.00 with an amount paid of $5.00 is 1 $1 bill.")
+
+print(optimal_change(100.00, 500) == "The optimal change for an item that costs $100.00 with an amount paid of $500.00 is 4 $100 bills.")
+
+print(optimal_change(20.00,120) == "The optimal change for an item that costs $20.00 with an amount paid of $120.00 is 5 $100 bills.")
+
+##EDGE CASES
+print(optimal_change(5,5) == "Thanks for using exact change.  Have a great day!")
+
+print(optimal_change(0.00,5.00) == "This item is on the house, keep your $5.00.")
+
+print(optimal_change(5.00,4.00) == "Sorry, you don't have enough money.  This item costs $5.00 and you only gave $4.00.")
+
+print(optimal_change(0.00,0.00) == "This item is on the house!")
+
+print(optimal_change(.01,100.00) == "The optimal change for an item that costs $0.01 with an amount paid of $100.00 is 1 $50 bill, 2 $20 bills, 1 $5 bill, 4 $1 bills, 3 quarters, 2 dimes, and 4 pennies.")
+
